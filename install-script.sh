51c21222a54f2c38a967b791eddae537f645145b
Jack Mead <jack.mead@corndel.com>
2024-08-19 17:11:26 +0100

    Simple install script

---
+++ install-script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir /agent && cd /agent
+
+wget https://vstsagentpackage.azureedge.net/agent/3.243.0/vsts-agent-linux-x64-3.243.0.tar.gz
+
+tar zxvf vsts-agent-linux-x64-3.243.0.tar.gz
+
+sudo ./config.sh --unattended --url $1 --auth pat --token $2 --pool default --agent myAgent –acceptTeeEula
+sudo ./svc.sh install
+sudo ./svc.sh start
