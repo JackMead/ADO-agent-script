784a5232a29f05fe9a8d8a179ac5bb89086dee08
Jack Mead <jack.mead@corndel.com>
2024-08-20 09:16:07 +0100

    Try fix the install script

--- install-script.sh
+++ install-script.sh
@@ -5,6 +5,6 @@ mkdir /agent && cd /agent
 wget https://vstsagentpackage.azureedge.net/agent/3.243.0/vsts-agent-linux-x64-3.243.0.tar.gz
 
 tar zxvf vsts-agent-linux-x64-3.243.0.tar.gz
-/agent/config.sh --unattended --url $1 --auth pat --token $2 --pool default --agent myAgent –acceptTeeEula
+/agent/config.sh --unattended --url $1 --auth pat --token $2 --pool default --agent myAgent --acceptTeeEula
 sudo /agent/svc.sh install
 sudo /agent/svc.sh start
